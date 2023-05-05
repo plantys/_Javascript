110a675f42f03ebb2ae39310647571dbb67bca1a
aryan <asin0100@student.monash.edu>
2023-05-05 20:41:53 +1000

    edited

--- react-javascript.js
+++ react-javascript.js
@@ -1,3 +1,40 @@
+// Destructuring with nested objects and default values------------------------------------------------------------
+const person1 = {
+  name: 'John',
+  age: 30,
+  address: {
+    street: '123 Main St',
+    city: 'New York',
+    country: 'USA',
+  },
+  hobbies: ['reading', 'movies'],
+};
+
+
+const {
+  name,
+  age,
+  address: { street, city, country },
+  job = 'Developer',
+  hobbies: [firstHobby, secondHobby],
+} = person1;
+
+console.log(name); // Output: 'John'
+console.log(age); // Output: 30
+console.log(street); // Output: '123 Main St'
+console.log(city); // Output: 'New York'
+console.log(country); // Output: 'USA'
+console.log(job); // Output: 'Developer' (default value)
+console.log(firstHobby); // Output: 'reading'
+console.log(secondHobby); // Output: 'movies'
+//-----------------------------------------------------------------------
+ 
+const transformToObjects = numberArray => numberArray.map(a => ({val: a}));
+const Array1 = [1, 2, 3];
+const objectArray = transformToObjects(Array1);
+
+console.log(objectArray); // Output: [{ val: 1 }, { val: 2 }, { val: 3 }]
+console.log('---------------')
 const numbers = [1,2,3]
 const doublenum = numbers.map(a=>a*2)
 console.log (doublenum);
